b33741c7656ce2cd389090659d84aadda7dfb6f8
RadiantK <pps8853@gmail.com>
2022-04-29 23:40:39 +0900

    관리자용 콘솔 클래스 생성
    관리자용 콘솔 클래스 생성

---
+++ jdbc-project/src/com/computers/console/admin/AdminConsole.java
@@ -0,0 +1,12 @@
+package com.computers.console.admin;
+
+import java.io.BufferedReader;
+
+import com.computers.dto.Member;
+
+public class AdminConsole {
+
+	public void adminFunction(BufferedReader br, Member member) {
+		System.out.println("관리자 전용 페이지 입니다.");
+	}
+}
