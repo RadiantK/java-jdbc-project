a086e9656864aa3f2373686bde8329bb93fcafeb
RadiantK <pps8853@gmail.com>
2022-04-29 23:43:41 +0900

    로그인에 대한 로직 처리
    로그인에 대한 비지니스 로직 처리

---
+++ jdbc-project/src/com/computers/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.computers.service;
+
+import com.computers.dao.MemberDao;
+import com.computers.dto.Member;
+import com.computers.exception.MemberNotFoundException;
+import com.computers.exception.WrongIdPasswordException;
+
+public class LoginService {
+
+	private MemberDao memberDao;
+	
+	public LoginService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+	public Member userLogin(String reqId, String reqPwd) {
+		Member member = memberDao.findByLoginId(reqId);
+		if(member == null) {
+			throw new MemberNotFoundException();
+		}
+		if(!member.getPwd().equals(reqPwd)) {
+			throw new WrongIdPasswordException();
+		}
+		
+		return member;
+	}
+	
+}
