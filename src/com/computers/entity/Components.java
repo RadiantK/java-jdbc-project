2d57be3b8b79cbea6205cf1818f53f262f64b043
RadiantK <pps8853@gmail.com>
2022-04-30 14:53:02 +0900

    Components 엔티티 클래스
    오라클 Components 테이블의 컬럼과 매핑 될 클래스

---
+++ src/com/computers/entity/Components.java
@@ -0,0 +1,74 @@
+package com.computers.entity;
+
+import java.sql.Date;
+
+public class Components {
+
+	private int cnum;
+	private String type;
+	private String cname;
+	private int price;
+	private Date regDate;
+	private int cnt;
+	
+	public Components() {}
+
+	public Components(int cnum, String type, String cname, int price, Date regDate, int cnt) {
+		super();
+		this.cnum = cnum;
+		this.type = type;
+		this.cname = cname;
+		this.price = price;
+		this.regDate = regDate;
+		this.cnt = cnt;
+	}
+
+	public int getCnum() {
+		return cnum;
+	}
+
+	public void setCnum(int cnum) {
+		this.cnum = cnum;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+
+}
