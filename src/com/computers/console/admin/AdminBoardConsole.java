026ff4f8ebc0803e1c84f7be92f21157fada8641
RadiantK <pps8853@gmail.com>
2022-05-01 19:23:38 +0900

    관리자 게시판 관리 클래스
    admin전용 게시판 콘솔 출력을 처리하는 클래스

---
+++ src/com/computers/console/admin/AdminBoardConsole.java
@@ -0,0 +1,50 @@
+package com.computers.console.admin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import com.computers.config.Config;
+import com.computers.entity.Member;
+import com.computers.service.BoardListService;
+
+public class AdminBoardConsole {
+	
+	public void boardFunction(BufferedReader br, Member member) {
+		Config config = Config.getInstance();
+		BoardListService service = config.getBoardListService();
+		
+		while(true) {
+			service.getBoardService();
+			try {
+				int menu = Integer.parseInt(br.readLine());
+				switch(menu) {
+					case 1:
+						service.prevPage();
+						break;
+					case 2:
+						service.nextPage();
+						break;
+					case 3:
+						service.search(br);
+						break;
+					case 4:
+						service.checkDetailBoard(br);
+						break;
+					case 5:
+						service.updateBoard(br, member);
+						break;
+					case 6:
+						service.adminRemoveBoard(br);
+						break;
+					case 7:
+						service.init();
+						System.out.println("이전으로 돌아갑니다.");
+						return;
+				}
+				
+			} catch (NumberFormatException | IOException e) {
+				System.out.println("잘못된 형식 입니다.");
+			}
+		}
+	}
+}
