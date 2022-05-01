e17557f677369059cc922e687f0fb46e582c86fe
RadiantK <pps8853@gmail.com>
2022-05-01 19:39:06 +0900

    PaymentDao 수정
    PaymentDao를 사용하는 환경에 맞게 수정함

--- src/com/computers/dao/PaymentDao.java
+++ src/com/computers/dao/PaymentDao.java
@@ -1,30 +1,42 @@
 package com.computers.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.computers.dto.PayInfoCommand;
+import com.computers.dto.PaymentRequest;
 import com.computers.entity.Payment;
 import com.computers.util.DataUtil;
 
 // 결제관련 데이터 처리
 public class PaymentDao {
 
-	// 결제 정보(관리자용)
-	public List<Payment> AllList() {
-		String sql = "SELECT * FROM payment ORDER BY regdate";
+	// 결제정보와 배송정보 통합(관리자용)
+	public List<PayInfoCommand> allList(int page, String type, String word) {
+		String sql = "SELECT * FROM ( "
+				+ "SELECT ROWNUM NUM, C.* FROM ( "
+				+ "SELECT p.pnum, p.id, p.cname, p.cnt, p.amount, p.means, p.status pstatus, "
+				+ "s.snum, s.sname, s.address, s.startdate, s.enddate, s.status sstatus "
+				+ "FROM payment p JOIN shippinginfo s ON p.pnum = s.pnum) C "
+				+ "WHERE "+type+" LIKE ?) "
+				+ "WHERE NUM BETWEEN ? AND ?";
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		List<Payment> list = new ArrayList<>();
+		List<PayInfoCommand> list = new ArrayList<>();
 		
 		try {
 			con = DataUtil.getConnection();
 			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, "%"+word+"%");
+			pstmt.setInt(2, 1+(page-1)*10);
+			pstmt.setInt(3, page*10);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
@@ -34,11 +46,19 @@ public class PaymentDao {
 				int cnt = rs.getInt("cnt");
 				int amount = rs.getInt("amount");
 				String means = rs.getString("means");
-				String status = rs.getString("status");
+				String pstatus = rs.getString("pstatus");
+				int snum = rs.getInt("snum");
+				String sname = rs.getString("sname");
+				String address = rs.getString("address");
+				Date startDate = rs.getDate("startdate");
+				Date endDate = rs.getDate("enddate");
+				String sstatus = rs.getString("sstatus");
 				
-				Payment payment = 
-						new Payment(pnum, id, cname, cnt, amount, means, status);
-				list.add(payment);
+				PayInfoCommand pay = new PayInfoCommand(
+						pnum, id, cname, cnt, amount, means, pstatus, snum,
+						sname, address, startDate, endDate, sstatus);
+				
+				list.add(pay);
 			}
 			return list;
 		}catch (SQLException e) {
@@ -50,9 +70,9 @@ public class PaymentDao {
 	}
 	
 	// 결제 정보(회원용)
-	public List<Payment> selsectList(Payment pay) {
+	public List<Payment> memberList(String memid, int month) {
 		String sql = "SELECT * FROM payment "
-				+ "WHERE id = ? AND regdate > sysdate - 100 ORDER BY regdate";
+				+ "WHERE id = ? AND regdate > sysdate - ? ORDER BY regdate DESC";
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -62,7 +82,8 @@ public class PaymentDao {
 		try {
 			con = DataUtil.getConnection();
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, pay.getId());
+			pstmt.setString(1, memid);
+			pstmt.setInt(2, month * 30);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
@@ -72,10 +93,11 @@ public class PaymentDao {
 				int cnt = rs.getInt("cnt");
 				int amount = rs.getInt("amount");
 				String means = rs.getString("means");
+				Date regDate = rs.getDate("regDate");
 				String status = rs.getString("status");
 				
 				Payment payment = 
-						new Payment(pnum, id, cname, cnt, amount, means, status);
+						new Payment(pnum, id, cname, cnt, amount, means, regDate, status);
 				list.add(payment);
 			}
 			return list;
@@ -110,16 +132,17 @@ public class PaymentDao {
 	}
 	
 	// 결제정보 삽입
-	public int insert(Connection con, Payment pay,int cnt) {
-		String sql = "INSERT INTO payment VALUES(SEQ_payment, ?,?,?,?,?)";
+	public int insert(Connection con, PaymentRequest pay) {
+		String sql = "INSERT INTO payment VALUES("
+				+ "SEQ_payment.nextval, ?, ?, ?, ?, ?, sysdate, '결재 완료')";
 		PreparedStatement pstmt = null;
 		
 		try {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, pay.getId());
 			pstmt.setString(2, pay.getCname());
-			pstmt.setInt(3, cnt);
-			pstmt.setInt(4, cnt*pay.getAmount());
+			pstmt.setInt(3, pay.getCnt());
+			pstmt.setInt(4, pay.getCnt()*pay.getPrice());
 			pstmt.setString(5, pay.getMeans());
 			
 			int result = pstmt.executeUpdate();
@@ -149,5 +172,56 @@ public class PaymentDao {
 		}finally {
 			DataUtil.close(pstmt);
 		}
+	}
+	
+	// 결제 취소
+	public int cancel(Connection con, int pnum) {
+		String sql = "UPDATE payment SET status = ? WHERE pnum = ?";
+		PreparedStatement pstmt = null;
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, "결제취소");
+			pstmt.setInt(2, pnum);
+			
+			int result = pstmt.executeUpdate();
+			return result;
+		}catch (SQLException e) {
+			System.out.println("회원정보 제거중 오류가 발생했습니다.");
+			return -1;
+		}finally {
+			DataUtil.close(pstmt);
+		}
+	}
+	
+	// 전체 결제정보 갯수(결제정보, 배송정보)
+	public int getCount(String type, String word) {
+		String sql = "SELECT COUNT(*) count FROM ( "
+				+ "SELECT p.pnum, p.id, p.cname, p.cnt, p.amount, p.means, p.status pstatus, "
+				+ "s.snum, s.sname, s.address, s.startdate, s.enddate, s.status sstatus "
+				+ "FROM payment p JOIN shippinginfo s ON p.pnum = s.pnum) C "
+				+ "WHERE "+type+" LIKE ?";
+		int count = 0;
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = DataUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, "%"+word+"%");
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("count");
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}finally {
+			DataUtil.close(con, pstmt, rs);
+		}
+		return count;
 	}
 }
