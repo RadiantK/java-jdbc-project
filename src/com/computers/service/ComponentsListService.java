748e9df532aee7aeaba3ed7a1e2fdd4ac9fb1654
RadiantK <pps8853@gmail.com>
2022-05-02 15:24:22 +0900

    사용하지 않는 메뉴 제거
    사용하지 않는 메뉴 제거

--- src/com/computers/service/ComponentsListService.java
+++ src/com/computers/service/ComponentsListService.java
@@ -38,7 +38,7 @@ public class ComponentsListService {
 		}
 		System.out.println("====================================================================================================================");
 		System.out.printf("                                                  %d / %d%n", page, lastPage);
-		System.out.println("                        1.이전페이지   2.다음페이지   3. 검색하기   4.부품추가   5.부품수정   6.돌아가기");
+		System.out.println("                                 1.이전페이지   2.다음페이지   3. 검색하기   4.돌아가기");
 	}
 
 	public void prevPage() {
@@ -88,74 +88,6 @@ public class ComponentsListService {
 		word = "";
 	}
 
-	
-	/*
-	 * 관리자용
-	 */
-	public void insertComponents(BufferedReader br) {
-		try {
-			System.out.println("부품종류를 입력하세요.");
-			String compType = br.readLine();
-			System.out.println("부품명을 입력하세요");
-			String compName = br.readLine();
-			System.out.println("부품 가격을 입력하세요.");
-			int compPrice = Integer.parseInt(br.readLine());
-			System.out.println("부품 입고일을 입력하세요[ex) 2020/01/01 ]");
-			String regDate = br.readLine();
-			System.out.println("부품 재고량을 입력하세요.");
-			int compCnt = Integer.parseInt(br.readLine());
-			
-			Components comp = new Components(0,
-					compType, compName, compPrice, null, compCnt);
-			
-			int n = componentsDao.insert(comp, regDate);
-			if(n < 1) {
-				System.out.println("등록에 실패했습니다. 중복되는 상품이 있는지 확인하세요.");
-				return;
-			}
-			System.out.println("제품이 등록되었습니다.");
-		} catch (NumberFormatException | IOException e) {
-			System.out.println("잘못된 양식으로 입력했습니다.");
-		}
-	}
 
-	public void updateComponents(BufferedReader br) {
-		try {
-			System.out.println("수정할 부품의 번호를 입력하세요");
-			int num = Integer.parseInt(br.readLine());
-			System.out.println("부품의 가격을 입력하세요");
-			int compPrice = Integer.parseInt(br.readLine());
-			System.out.println("부품의 재고량을 입력하세요");
-			int compCnt = Integer.parseInt(br.readLine());
-			
-			Components comp = new Components(
-					num, "", "", compPrice, null, compCnt);
-			
-			int n = componentsDao.editComp(comp);
-			if(n < 1) {
-				System.out.println("제품 수정에 실패했습니다. 제품번호를 다시 확인하세요.");
-				return;
-			}
-			System.out.println("제품이 수정되었습니다.");
-		} catch (NumberFormatException | IOException e) {
-			System.out.println("잘못된 양식의 입력입니다.");
-		}
-	}
-	
-	public void removeComponents(BufferedReader br) {
-		try {
-			System.out.println("제거할 부품의 번호를 입력해주세요.");
-			int num = Integer.parseInt(br.readLine());
-			
-			int n = componentsDao.remove(num);
-			if(n < 1) {
-				System.out.println("제품 제거에 실패했습니다. 제품번호를 다시 확인하세요.");
-				return;
-			}
-			System.out.println("제품이 제거되었습니다.");
-		}catch (NumberFormatException | IOException e) {
-			System.out.println("잘못된 양식의 입력입니다.");
-		}
-	}
 
 }
